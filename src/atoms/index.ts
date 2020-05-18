65e29cc589af254ffc6f833e0eb6fb3eba696755
Akira Kanno <teinen.github@gmail.com>
2020-05-18 09:49:04 +0900

    Add filter state

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,4 +1,4 @@
-import { atom } from 'recoil'
+import { atom, selector } from 'recoil'
 
 export interface ITodoItem {
   id: number
@@ -6,11 +6,39 @@ export interface ITodoItem {
   isComplete: boolean
 }
 
+export type TodoListFilterType =
+  | 'Show All'
+  | 'Show Completed'
+  | 'Show Uncompleted'
+
 const todoListState = atom<ITodoItem[]>({
   key: 'todoListState',
   default: []
 })
 
+const todoListFilterState = atom<TodoListFilterType>({
+  key: 'todoListFilterState',
+  default: 'Show All'
+})
+
+const filteredTodoListState = selector({
+  key: 'filteredTodoListState',
+  get: ({ get }) => {
+    const list = get(todoListState)
+    const filter = get(todoListFilterState)
+
+    switch(filter) {
+      case 'Show Completed':
+        return list.filter(item => item.isComplete)
+      case 'Show Uncompleted':
+        return list.filter(item => !item.isComplete)
+      default:
+        return list
+    }
+  }
+})
+
 export {
-  todoListState
+  todoListState,
+  filteredTodoListState
 }
